84dee2c677b843192adb03e173b493938445ae65
Zaccheaus <zaccheausyisa00@gmail.com>
2023-04-19 06:39:55 -0700

    feat: added anime description Endpoint

--- server.js
+++ server.js
@@ -5,8 +5,10 @@
 const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
@@ -32,7 +34,7 @@ app.get("/genres", async (request, response) => {
       link: link,
     });
   });
-
+  response.set("Acess-Control-Allow-Origin", "*");
   response.send(genreList);
 });
 
@@ -55,6 +57,58 @@ app.get("/gl", async (request, response) => {
 
     animeList.push({ title, image, link, released });
   });
+  response.set("Acess-Control-Allow-Origin", "*");
 
   response.send(animeList);
+});
+
+/**@example http://localhost:3000/desc/kekkon-yubiwa-monogatari */
+app.get("/desc/:title", async (request, response) => {
+  console.log(request.params.title);
+  const { data } = await axios.get(
+    `https://www3.gogoanimes.fi/category/${request.params.title}`
+  );
+  const $ = await cheerio.load(data);
+
+  const name = $(".anime_info_body h1").text();
+  const image = $(".anime_info_body img").attr("src");
+  const type = $("p.type").eq(0).find("a").text();
+  const summary = $("p.type").eq(1).text();
+
+  const genres = [];
+  $("p.type")
+    .eq(2)
+    .find("a")
+    .each((_i, el) => {
+      const genre = $(el).text();
+      const link = $(el).attr("href");
+
+      genres.push({ genre, link });
+    });
+
+  const release = $("p.type").eq(3).text();
+  const status = $("p.type").eq(4).find("a").text();
+
+  const availableEpisodes = [];
+  $("ul#episode_page li").each((_i, el) => {
+    const start_episode = $(el).find("a").attr("ep_start");
+    const end_episode = $(el).find("a").attr("ep_end");
+
+    availableEpisodes.push({ start_episode, end_episode });
+  });
+
+  const id = $("input#movie_id").attr("value");
+
+  response.set("Access-Control-Allow-Origin", "*");
+  response.send({
+    id,
+    name,
+    image,
+    type,
+    summary,
+    genres,
+    release,
+    status,
+    availableEpisodes,
+  });
 });
